399333318b4552fae65d3d3e4d55ef84e4105228
joebalzac <jgbalzac@gmail.com>
2021-06-25 15:24:55 -0400

    Fundamental -2

--- Fundamentals - 02/script.js
+++ Fundamentals - 02/script.js
@@ -9,37 +9,65 @@
 // // const interface = 'Audio'
 // const private = 534
 
-function logger() {
-    console.log('My name is Jonas')
+// function logger() {
+//     console.log('My name is Jonas')
+// }
+
+// //calling / running / invoking function 
+// logger()
+// logger()
+// logger()
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice
+// }
+
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice)
+
+// const num = Number('23')
+// console.log(num)
+
+
+// function fruitSmoothie (bannanas, strawberries) {
+//     const coldSmoothie = `This is a smoothie with ${bannanas} bannanas and ${strawberries} strawberries`
+//     return coldSmoothie
+// }
+
+// const storeSmoothie = fruitSmoothie(6, 9)
+// console.log(storeSmoothie)
+
+// const newyorkSmoothie = fruitSmoothie(420, 'blazeit')
+// console.log(newyorkSmoothie)
+
+
+//Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear
+}
+const age1 = calcAge1(1985)
+
+//Function expression
+const calcAge2 = function(birthYear){
+    return 2037 - birthYear
+}
+const age2 = calcAge2(1985)
+console.log(age1, age2)
+
+//Arrow Function 
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(1985)
+console.log(age3)
+
+const yearsUntilRetirment = (birthYear, firstName) => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age
+    return `${firstName} retires in ${retirement} years`
 }
 
-//calling / running / invoking function 
-logger()
-logger()
-logger()
-
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
-    return juice
-}
-
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
-
-const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
-
-const num = Number('23')
-console.log(num)
-
-
-function fruitSmoothie (bannanas, strawberries){
-    const coldSmoothie = `This is a smoothie with ${bannanas} bannanas and ${strawberries} strawberries`
-    return coldSmoothie
-}
-
-const storeSmoothie = fruitSmoothie(6, 9)
-console.log(storeSmoothie)
-
-const newyorkSmoothie = fruitSmoothie(420, 'blazeit')
-console.log(newyorkSmoothie)
+console.log(yearsUntilRetirment(1985, 'Joe'))
+console.log(yearsUntilRetirment(1991, 'Bob'))
