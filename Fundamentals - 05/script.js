0560c18e2d4634530dcf995adbea7a9e960790db
joebalzac <jgbalzac@gmail.com>
2021-07-16 16:19:28 -0400

    guessing game

--- Fundamentals - 05/script.js
+++ Fundamentals - 05/script.js
@@ -1,1 +1,56 @@
-console.log('hello world')
+'use script'
+ 
+// console.log(document.querySelector('.message').textContent)
+// document.querySelector('.message').textContent = 'Correct Number'
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+
+// document.querySelector('.guess').value = 23
+// console.log(document.querySelector('.guess').value)
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+let score = 20 
+
+
+const checkButton = document.querySelector('.check')
+
+checkButton.addEventListener('click', () => {
+   const guess = Number(document.querySelector('.guess').value)
+    console.log(guess, typeof guess)
+
+    //When there is no limit
+    if(!guess) {
+        document.querySelector('.message').textContent = '📛 No Number!'
+
+    //When player wins
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉 Correct Number!'
+        document.querySelector('.number').textContent = secretNumber
+        
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+
+    //When number is too high     
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '📈 Too high!'
+        score-- 
+        document.querySelector('.score').textContent = score
+        } else {
+            document.querySelector('.message').textContent = "you lost the game 😫"
+            document.querySelector('.score').textContent = 0
+        }
+    
+    //When number is too low    
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+        document.querySelector('.message').textContent = '📉 Too low!'
+        score--
+        document.querySelector('.score').textContent = score
+    } else {
+        document.querySelector('.message').textContent = "you lost the game 😫"
+        document.querySelector('.score').textContent = 0
+    }
+}})
