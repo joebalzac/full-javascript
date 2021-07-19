8e3c840d94c525838c305a3050c928ce1c67bf71
joebalzac <jgbalzac@gmail.com>
2021-07-19 00:23:22 -0400

    modal

---
+++ Fundamental - 07/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//Variables
+const btnShowModal = document.querySelectorAll('.show-modal')
+const btnCloseModal = document.querySelector('.close-modal')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay') 
+
+
+//Functions Open & Close mdoal 
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+        
+//Loop through showModal
+for (let i = 0; i < btnShowModal.length; i++)
+btnShowModal[i].addEventListener('click', openModal)
+
+//close button & close overlay
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
