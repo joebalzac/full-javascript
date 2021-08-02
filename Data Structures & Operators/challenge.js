317cd684a5ef16032a8a45f388aaa6b328d5f1a1
joebalzac <jgbalzac@gmail.com>
2021-08-02 09:07:40 -0400

    new

--- Data Structures & Operators/challenge.js
+++ Data Structures & Operators/challenge.js
@@ -13,7 +13,7 @@ const game = {
       "Coman",
       "Muller",
       "Gnarby",
-      "Lewandowski",
+      "Lewandowski"
     ],
     [
       "Burki",
@@ -26,18 +26,28 @@ const game = {
       "Hazard",
       "Brandt",
       "Sancho",
-      "Gotze",
-    ],
+      "Gotze"
+    ]
   ],
   score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  scored: [
+    "Lewandowski",
+    "Bob Marley",
+    "Gnarby",
+    "Lewandowski",
+    "Hummels",
+    "Bob Marley"
+  ],
   date: "Nov 9th, 2037",
   odds: {
     team1: 1.33,
     x: 3.25,
-    team2: 6.5,
-  },
+    team2: 6.5
+  }
 };
+
+for (const [i, player] of game.scored.entries());
+console.log(` Goal ${(i, 1)}: ${}`);
 
 // //1.
 // const [players1, players2] = game.players;
@@ -73,27 +83,29 @@ const game = {
 // team1 < team2 && console.log("Team1 will most likely win");
 // team1 > team2 && console.log("Team2 will most likely win");
 
-for (const [i, player] of game.scored.entries())
-  console.log(`Goal ${i + 1}: ${player}`);
+// //1.
+// for (const [i, player] of game.scored.entries())
+//   console.log(`Goal ${i + 1}: ${player}`);
 
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
+// //2.
+// const odds = Object.values(game.odds);
+// let average = 0;
+// for (const odd of odds) average += odd;
+// average /= odds.length;
+// console.log(average);
 
-//3.
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === "x" ? "draw" : `Victory of ${game[team]}`;
-  console.log(`Odd of ${teamStr} ${odd}`);
-}
+// //3.
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === "x" ? "draw" : `Victory of ${game[team]}`;
+//   console.log(`Odd of ${teamStr} ${odd}`);
+// }
 
-// //Bonus**
-const scorers = {};
-for (const player of game.scored) {
-  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-  console.log(scorers);
-}
+// // //Bonus**
+// const scorers = {};
+// for (const player of game.scored) {
+//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+//   console.log(scorers);
+// }
 
 // const scorers = {};
 // for (const player of game.scored) {
